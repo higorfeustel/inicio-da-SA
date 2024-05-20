acd5f055a724eab75f2b0fdd26f13932b5d832b4
JulioLms4 <julio_lemos@estudante.sesisenai.org.br>
2024-05-20 10:50:19 -0300

    teste

--- SA/Script/cadastro.js
+++ SA/Script/cadastro.js
@@ -15,3 +15,15 @@ function salvarDados(){
     console.log(usuario)
 }
 
+
+
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+  }
+  
+  /* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+  }
